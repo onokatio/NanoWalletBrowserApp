778a4a6d572428b5f77b95c3e8b7426488ed6569
onokatio <onokatio@gmail.com>
2016-12-31 15:26:31 +0900

    move

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('src/mijin.io/nanowallet/start.html', {
-    'outerBounds': {
-      'width': 713,
-      'height': 543
-    }
-  });
-});
-
